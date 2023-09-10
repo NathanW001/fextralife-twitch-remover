1daf3d3e0d81cf98b1866219e20059017f08832c
NathanW001 <nathan.wootton1@gmail.com>
2023-09-10 00:52:40 -0400

    removed files from root

--- remove_html.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", handleLoad())
-
-function handleLoad() {
-  const wrapper = document.getElementById("wrapper");
-  const sidebar_wrapper = document.getElementById("sidebar-wrapper");
-
-  wrapper.style.setProperty("padding-left", 0)
-  sidebar_wrapper.remove()
-}

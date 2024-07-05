c9aeea8dc68f475daa7ea2f541bde0bd2214501c
NathanW001 <nathan.wootton1@gmail.com>
2024-07-04 21:14:41 -0400

    fixed breaking search on site

--- src/remove_html.js
+++ src/remove_html.js
@@ -1,5 +1,15 @@
 addEventListener("DOMContentLoaded", () => {
   document.getElementById("wrapper").style.setProperty("padding-left", 0);
   document.getElementById("wrapper").style.setProperty("transition", "0s");
-  document.getElementById("sidebar-wrapper").remove();
+
+  // // Cannot remove element outright, it breaks the search feature somehow
+  // document.getElementById("sidebar-wrapper").remove(); 
+
+  let parent = document.getElementById("sidebar-wrapper");
+  parent.querySelector(".row").remove();
+  let to_remove = parent.querySelectorAll(".sidebar-nav > div:not(.hidden-lg)")
+  to_remove.forEach((item) => {
+    item.remove();
+  });
+  parent.style.setProperty("width", "0px")
 });
